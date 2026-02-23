21a690710662a24936808b47bae3de83bc173ec2
IlhamSoejudAlkahfiardy <ilhamyusuf386@gmail.com>
2026-02-23 18:43:11 +0700

    update kedua

--- app/page.tsx
+++ app/page.tsx
@@ -123,11 +123,11 @@ export default function Home() {
               <CardHeader className="space-y-5">
                 <div className="space-y-2">
                   <CardTitle className="text-slate-100">Jumlah Donatur</CardTitle>
-                  <CardDescription className="text-green-400">3</CardDescription>
+                  <CardDescription className="text-green-400">4</CardDescription>
                 </div>
                 <div className="space-y-2">
                   <CardTitle className="text-slate-100">Jumlah Donasi</CardTitle>
-                  <CardDescription className="text-green-400">Rp {formatNumber(705000)}</CardDescription>
+                  <CardDescription className="text-green-400">Rp {formatNumber(805000)}</CardDescription>
                 </div>
                 <div className="space-y-2">
                   <CardTitle className="text-slate-100">Jumlah Pengeluaran</CardTitle>
@@ -135,12 +135,12 @@ export default function Home() {
                 </div>
                 <div className="space-y-2">
                   <CardTitle className="text-slate-100">Sisa Keuangan</CardTitle>
-                  <CardDescription className="text-green-400">Rp {formatNumber(705000)}</CardDescription>
+                  <CardDescription className="text-green-400">Rp {formatNumber(805000)}</CardDescription>
                 </div>
               </CardHeader>
               <CardFooter className="flex flex-col justify-start items-start gap-5">
                 <p className="text-xs font-light text-slate-100">
-                  Data per tanggal 23-02-26 - 14:31 WIB
+                  Data per tanggal 23-02-26 - 18:42 WIB
                 </p>
               </CardFooter>
             </Card>
