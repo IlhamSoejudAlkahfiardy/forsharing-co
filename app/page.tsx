c4dc54bac74fec10c9c893ee6fe0773c0ac7628c
IlhamSoejudAlkahfiardy <ilhamyusuf386@gmail.com>
2026-02-25 08:22:47 +0700

    update kedelapan

--- app/page.tsx
+++ app/page.tsx
@@ -42,11 +42,11 @@ export default function Home() {
   const [selectedDonation, setSelectedDonation] = useState<Donation | null>(null)
   const [page, setPage] = useState<Page>("report");
   const [dataReport, setDataReport] = useState<DataReport>({
-    donatur: 21,
-    totalDonasi: 1923800,
+    donatur: 25,
+    totalDonasi: 2443800,
     totalPengeluaran: 0,
-    sisaKeuangan: 1923800,
-    tanggal: "24-02-26 - 22:18 WIB",
+    sisaKeuangan: 2443800,
+    tanggal: "25-02-26 - 08:22 WIB",
   });
   const searchDonationsByNameAmountNotes = donations.filter((item) => {
     return item.name.toLowerCase().includes(search.toLowerCase()) || item.amount.toString().includes(search.toLowerCase()) || item.notes.toLowerCase().includes(search.toLowerCase());
